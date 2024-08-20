6e04edf0a1b079b75ed2e197b788c498f27762ff
gabrielgreco1 <gabrielargreco@gmail.com>
2024-08-20 03:09:13 -0300

    feat: Get ALL player stats

--- src/main.js
+++ src/main.js
@@ -1,6 +1,6 @@
 import { scrapeAndSave } from './services/scrapeService.js';
 
-const url = 'https://fbref.com/pt/equipes/abdce579/2024/partidas/c24/schedule/Palmeiras-Resultados-e-Calendarios-Serie-A';
-const homeTeam = 'Palmeiras';   
+const url = 'https://fbref.com/pt/equipes/6f7e1f03/2024/partidas/c24/schedule/Internacional-Resultados-e-Calendarios-Serie-A';
+const homeTeam = 'Internacional';   
 
 scrapeAndSave(url, homeTeam);
--- src/services/scrapeService.js
+++ src/services/scrapeService.js
@@ -4,6 +4,7 @@ import { logMessage } from '../utils/logger.js';
 import { extractMatches } from './extractMatches.js';
 import { extractEventDetails } from './extractMatchDetails/events.js';
 import { extractStatsExtra } from './extractMatchDetails/statsExtra.js';
+import { extractPlayerStats } from './extractMatchDetails/playerStats.js'; // Importa a função criada
 
 export async function scrapeAndSave(url) {
     const userAgent = 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.124 Safari/537.36';
@@ -29,10 +30,14 @@ export async function scrapeAndSave(url) {
                 const events = extractEventDetails(matchHtml, match.homeTeam, match.awayTeam);
                 const statsExtra = extractStatsExtra(matchHtml, match.homeTeam, match.awayTeam);
                 
-                const finalData = [
-                    { events },
-                    { statsExtra }
-                ];
+                // Chama a função para extrair as estatísticas dos jogadores
+                const playerStats = extractPlayerStats(matchHtml);
+
+                const finalData = {
+                    events,
+                    statsExtra,
+                    players: playerStats // Adiciona as estatísticas dos jogadores no JSON final
+                };
 
                 saveMatchToJSON(homeTeam.replace(/\s+/g, '_').toUpperCase(), `${match.homeTeam}vs${match.awayTeam}.json`, finalData);
                 await sleep(2000); // Delay para evitar sobrecarga do servidor
